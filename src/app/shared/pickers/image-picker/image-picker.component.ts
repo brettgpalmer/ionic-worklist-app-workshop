fe6c1ded0c1ddf4d2bb37aeccc8a549b06a6a549
Brett Palmer <brettgpalmer@gmail.com>
2019-04-08 08:21:45 -0600

    Added image picker but still have errors loading in new work detail page

---
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Plugins, Capacitor, CameraSource, CameraResultType } from '@capacitor/core';
+
+@Component({
+  selector: 'app-image-picker',
+  templateUrl: './image-picker.component.html',
+  styleUrls: ['./image-picker.component.scss'],
+})
+export class ImagePickerComponent implements OnInit {
+
+  @Output() imagePick = new EventEmitter<string>();
+  @Input() showPreview = false;
+  selectedImage: String;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  /* 
+    method to selectd an image but also provide method if camera is not available
+  */
+  onPickImage() {
+
+    if( !Capacitor.isPluginAvailable('Camera')) {
+      return;
+    }
+    Plugins.Camera.getPhoto({
+      quality: 50,
+      source: CameraSource.Prompt,
+      correctOrientation: true,
+      width: 300,
+      resultType: CameraResultType.Base64
+    }).then( image => {
+     this.selectedImage = image.base64Data;
+     this.imagePick.emit(image.base64Data) ;
+
+    }).catch(error => {
+      console.log(error);
+      return false;
+    });
+  }
+}
