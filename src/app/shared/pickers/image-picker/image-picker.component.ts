60dd6ff457d704161ea703f86d30e06133ef959f
Brett Palmer <brettgpalmer@gmail.com>
2019-04-09 08:50:40 -0600

    Updates to image-picker to allow someone to select a file

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
 import { Plugins, Capacitor, CameraSource, CameraResultType } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -8,20 +9,39 @@ import { Plugins, Capacitor, CameraSource, CameraResultType } from '@capacitor/c
 })
 export class ImagePickerComponent implements OnInit {
 
+  @ViewChild('filePicker') filePickerRef: ElementRef<HTMLInputElement>;
   @Output() imagePick = new EventEmitter<string>();
   @Input() showPreview = false;
   selectedImage: String;
+  usePicker = false;
 
-  constructor() { }
+  constructor(private platform: Platform) { }
 
-  ngOnInit() {}
+  // Check the platform types for debugging
+  ngOnInit() {
+    console.log('Mobile', this.platform.is('mobile'));
+    console.log('Capacitor', this.platform.is('capacitor'));
+    console.log('Hybrid', this.platform.is('hybrid'));
+    console.log('iOS', this.platform.is('ios'));
+    console.log('Android', this.platform.is('android'));
+    console.log('Desktop', this.platform.is('desktop'));
+    console.log('Electron', this.platform.is('electron'));
+
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
+      this.usePicker = true;
+    }
+  }
 
   /* 
     method to selectd an image but also provide method if camera is not available
   */
   onPickImage() {
 
-    if (!Capacitor.isPluginAvailable('Camera')) {
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker ) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -36,7 +56,25 @@ export class ImagePickerComponent implements OnInit {
 
     }).catch(error => {
       console.log(error);
+      if (this.usePicker) {
+        this.filePickerRef.nativeElement.click();
+      }
       return false;
     });
+  }
+
+  onFileChosen(event: Event) {
+    console.log('here is the event: ' + event);
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+    }
+    fr.readAsDataURL(pickedFile);
   }
 }
